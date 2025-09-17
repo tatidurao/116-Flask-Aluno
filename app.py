16fe94eeefca484bdb9bec7adaa57d8ba63953a6
Tátila Bianca Durão Fernandes <91564423+tatidurao@users.noreply.github.com>
2025-09-17 13:54:11 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,1 @@
-from flask import Flask, render_template
 
-app = Flask(__name__)
-
-@app.route("/index")
-def primeira_pagina():
-    name = "Hector"
-    return render_template("index.html", index_variable=name)
-
-@app.route("/sobre-flask")
-def sobre_flask():
-    return render_template("sobre_flask.html")
-
-if __name__ == "__main__":
-    app.run(debug=True)
